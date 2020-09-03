ec07fac0e83edeb6c5b2a980b128a0527e010503
Johannes Filter <hi@jfilter.de>
2020-09-03 18:51:04 +0200

    adapt to new api

--- utils/networking.js
+++ utils/networking.js
@@ -1,52 +1,31 @@
 import ky from "ky-universal";
 
+const API_LOCATION = "http://localhost:8000";
+// const API_LOCATION = "https://api.rechtegewalt.info";
+
 async function fetchOptions(q = null, startDate = null, endDate = null) {
   let whereClause = "";
 
   if (q != null) {
-    whereClause = `+where+title+like+'%25${q}%25'`;
+    whereClause += `?q=${q}'`;
   }
 
-  let url = `https://data.rechtegewalt.info/rechtegewalt.json?sql=select+id%2C+title%2C+subdivisions%2C+date+from+incidents+${whereClause}`;
-
+  const url = `${API_LOCATION}/autocomplete/${whereClause}`;
   const apiResponse = await ky.get(url).json();
-
-  const optionsArray = apiResponse.rows
-    .map((x) => x[1])
-    .filter((x) => x != null);
-
-  const optionsSet = new Set();
-  optionsArray.forEach((x) => {
-    x.split(" ").forEach((xx) => optionsSet.add(xx));
-  });
-
-  return Array.from(optionsSet);
+  return apiResponse.results.map((x) => x.string);
 }
 
 async function fetchGeoData(q = null, startDate = null, endDate = null) {
   let whereClause = "";
 
   if (q != null) {
-    whereClause = `+where+title+like+'%25${q}%25'`;
+    whereClause += `?q=${q}'`;
   }
 
-  let url = `https://data.rechtegewalt.info/rechtegewalt.json?sql=select+count%28*%29%2C+subdivisions%2C%0D%0A++AsGeoJSON%28point_geom%29%0D%0Afrom%0D%0A+incidents+${whereClause}+group+by+subdivisions`;
+  let url = `${API_LOCATION}/aggincidents/${whereClause}`;
 
   const apiResponse = await ky.get(url).json();
-  const features = apiResponse.rows.map((x) => {
-    return {
-      geometry: JSON.parse(x[2]),
-      properties: { count: x[0], location: x[1] },
-      type: "Feature",
-    };
-  });
-
-  const data = {
-    type: "FeatureCollection",
-    features,
-  };
-
-  return data;
+  return apiResponse;
 }
 
 async function fetchData(q = null) {
