bc39d67e74f951f0be0426f0ca941926d9c79740
Johannes Filter <hi@jfilter.de>
2020-09-18 11:02:25 +0200

    dont show lakes / landuse

--- components/Map/index.js
+++ components/Map/index.js
@@ -245,7 +245,8 @@ class Map extends Component {
           {...viewport}
           width="50%"
           height="100%"
-          mapStyle="mapbox://styles/jfilter/ckf7yh70g01i11ao1uo2ozug0"
+          mapStyle="mapbox://styles/jfilter/ckf80h3h2521o19pfe9sam2cq"
+          // mapStyle="mapbox://styles/jfilter/ckf7yh70g01i11ao1uo2ozug0"
           // mapStyle="http://168.119.114.9:8080/styles/positron/style.json"
           onViewportChange={this._onViewportChange}
           mapboxApiAccessToken={MAPBOX_TOKEN}
