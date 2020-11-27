125efae948c3324b9a31fda9d0c00348321c46bc
Johannes Filter <hi@jfilter.de>
2020-11-27 23:08:40 +0100

    use cms for hintergrund

--- pages/hintergrund.js
+++ pages/hintergrund.js
@@ -1,29 +1,30 @@
 import Head from "next/head";
+
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import { fetchContent } from "../utils/networking";
 
-export default function Home() {
+function Hintergrund({ content }) {
+  const title = content.meta.seo_title;
+  const description = content.meta.search_description;
+
   return (
     <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <NavBar />
-      <section className="hero">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">Hero title</h1>
-            <h2 className="subtitle">Hero subtitle</h2>
-          </div>
-        </div>
-      </section>
-      <section className="section">
-        <div className="container">
-          <h1 className="title">Section</h1>
-          <h2 className="subtitle">
-            A simple container to divide your page into{" "}
-            <strong>sections</strong>, like the one you're currently reading
-          </h2>
-        </div>
-      </section>
+      <div dangerouslySetInnerHTML={{ __html: content.body }} />
       <Footer />
     </>
   );
 }
+
+export async function getServerSideProps() {
+  const content = await fetchContent("hintergrund");
+  return { props: { content } };
+}
+
+export default Hintergrund;
--- pages/index.js
+++ pages/index.js
@@ -16,24 +16,6 @@ function Home({ content }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <NavBar />
-      {/* <section className="hero">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">Hero title</h1>
-            <h2 className="subtitle">Hero subtitle</h2>
-          </div>
-        </div>
-      </section>
-      <section className="section">
-        <div className="container">
-          <h1 className="title">Section</h1>
-          <h2 className="subtitle">
-            A simple container to divide your page into
-            <strong>sections</strong>, like the one you're currently reading
-          </h2>
-        </div>
-      </section> */}
-
       <div dangerouslySetInnerHTML={{ __html: content.body }} />
       <Footer />
     </>
