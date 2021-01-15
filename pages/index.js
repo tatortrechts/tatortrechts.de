0aad39fcb9172e53b1c16d9a14c49ba842f7298f
Johannes Filter <hi@jfilter.de>
2021-01-15 14:29:05 +0100

    remove hard coded stuff

--- pages/index.js
+++ pages/index.js
@@ -15,18 +15,6 @@ function Home({ content }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <NavBar />
-      <div style={{ position: "relative" }}>
-        <img src="/strasse.png" style={{ width: "100%" }}></img>
-        {/* <video playsinline autoplay loop mute>
-          <source src="/strasse.mov" type="video/mp4" />
-        </video> */}
-        <div style={{ position: "absolute", top: "500px", left: "500px" }}>
-          <h1 class="title has-text-white is-size-1">Tatort Rechts</h1>
-          <h2 class="subtitle has-text-white is-size-1">
-            Werkzeug für digitale Recherche zu Rechter Gewalt
-          </h2>
-        </div>
-      </div>
       <div dangerouslySetInnerHTML={{ __html: content.body }} />
       <Footer />
     </>
