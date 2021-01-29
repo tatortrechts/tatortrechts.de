390794cad77761f832d3039a118bcd4cb78c1c37
Johannes Filter <hi@jfilter.de>
2021-01-29 21:21:09 +0100

    refactor incident box

--- components/Map/IncidentList.js
+++ components/Map/IncidentList.js
@@ -1,22 +1,8 @@
 import * as dayjs from "dayjs";
 import { useRef, useState } from "react";
 import InfiniteScroll from "react-infinite-scroller";
-import { extractShortAddress } from "../../utils/labels";
 import Histogram from "./Histogram";
-
-const Source = ({ name, url, date }) => {
-  return (
-    <span>
-      {name}
-      {date && dayjs(date).format(", DD.MM.YYYY")}
-      {url && url.length > 0 && (
-        <a href={url} className="is-link">
-          Link
-        </a>
-      )}
-    </span>
-  );
-};
+import IncidentBox from "./IncidentBox";
 
 const Onboarding = ({ hideOnboarding }) => {
   return (
@@ -170,99 +156,9 @@ const IncidentList = ({
               hasMore={next != null}
             >
               {results &&
-                results.map((x) => {
-                  return (
-                    <div
-                      className="card has-text-dark"
-                      key={x.id}
-                      onMouseEnter={() =>
-                        setHighlight(x.location.geolocation.coordinates)
-                      }
-                      onMouseLeave={() => setHighlight(null)}
-                    >
-                      <header className="card-header">
-                        <p className="card-header-title">
-                          {extractShortAddress(x.location)},{" "}
-                          {dayjs(x.date).format("DD.MM.YYYY")}
-                        </p>
-                      </header>
-                      <div className="card-content">
-                        <div className="columns">
-                          <div className="column is-two-thirds">
-                            {(x.title || x.title_highlighted) && (
-                              <p
-                                className="content mb-3 has-text-weight-semibold"
-                                dangerouslySetInnerHTML={{
-                                  __html: x.title || x.title_highlighted,
-                                }}
-                              ></p>
-                            )}
-                            <p
-                              className="content"
-                              dangerouslySetInnerHTML={{
-                                __html:
-                                  x.description || x.description_highlighted,
-                              }}
-                            ></p>
-                          </div>
-                          <div className="column content is-size-7">
-                            <div
-                              className="p-3"
-                              style={{ backgroundColor: "#fee0d2" }}
-                            >
-                              {x.location.house_number && (
-                                <div>Hausnummer: {x.location.house_number}</div>
-                              )}
-                              {x.location.street && (
-                                <div>Straße: {x.location.street}</div>
-                              )}
-                              {x.location.city && (
-                                <div>Ort: {x.location.city}</div>
-                              )}
-                              {x.location.district && (
-                                <div>Ortsteil: {x.location.district}</div>
-                              )}
-                              {x.location.county && (
-                                <div>Landkreis: {x.location.county}</div>
-                              )}
-                              <div>
-                                {x.sources.length === 1 && (
-                                  <span>Quelle (aus Chronik): </span>
-                                )}
-                                {x.sources.length > 1 && (
-                                  <span>Quellen (aus Chronik): </span>
-                                )}
-                                {x.sources.map((x) => (
-                                  <Source
-                                    name={x.name}
-                                    url={x.url}
-                                    date={x.date}
-                                    key={x.name + x.date + x.url}
-                                  />
-                                ))}
-                              </div>
-
-                              <div>
-                                Chronik: {x.chronicle.name} (
-                                <a href={x.url}>Link</a>)
-                              </div>
-                              <br></br>
-                              <div>
-                                <div>
-                                  Ortsangaben aus Chronik: {x.orig_city}
-                                </div>
-                              </div>
-                              {x.tags && <div>Tags: {x.tags}</div>}
-                              {x.contexts && <div>Kontext: {x.contexts}</div>}
-                              {x.factums && <div>Handlung: {x.factums}</div>}
-                              {x.motives && <div>Motiv: {x.motives}</div>}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
+                results.map((x) => (
+                  <IncidentBox x={x} setHighlight={setHighlight} />
+                ))}
             </InfiniteScroll>
           </div>
         )}
