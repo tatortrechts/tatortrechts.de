33c893f19b69c75578e1d48aada9be95cbf0a804
Johannes Filter <hi@jfilter.de>
2021-02-05 19:50:47 +0100

    new landing page

--- pages/index.js
+++ pages/index.js
@@ -137,13 +137,16 @@ function Home({ content }) {
           <ContentMiddle>
             {/* <h4 className="title is-4">Rechte Gewalt vor deiner Haustür</h4> */}
             <h1 className="title is-2">
-              Fast <b className="is-size-1"> 16.000</b> rechte Gewalttaten
+              <b className="is-size-1">12</b> Projekte haben seit
             </h1>
             <h1 className="title is-2">
-              haben <b className="is-size-1">12</b> Organisationen
+              dem Jahr <b className="is-size-1">2000</b> fast
             </h1>
             <h1 className="title is-2">
-              seit dem Jahr <b className="is-size-1">2000</b> registriert.
+              <b className="is-size-1"> 16.000</b> Fälle mit
+            </h1>
+            <h1 className="title is-2">
+              <b className="is-size-1">rechtem </b> Bezug registriert.
             </h1>
           </ContentMiddle>
         </section>
@@ -153,10 +156,14 @@ function Home({ content }) {
             <h2 className="title is-4">Rechte Gewalt vor deiner Haustür</h2>
 
             <p>
-              LAUT der Antonio-Amadeu-Stiftung starben seit der Wende 213
-              Menschen durch rechte Gewalt (Stand 15.01.2021). Rechte Gewalt
-              passiert überall. Wenn du wissen willst wo, dann nutz unser
-              Recherche-Tool.
+              Hinter <a href="/ueber">Tatort Rechts</a> stehen{" "}
+              <a href="blog/team">Johannes Filter und Anna Neifer</a>. Seit
+              Sommer 2020 arbeiten wir an diesem Projekt, haben vieles verworfen
+              und am Ende liest du jetzt diese Zeilen. Mit{" "}
+              <a href="/blog">Tatort Rechts</a> wollen wir einen Schwerpunkt
+              setzen zu rechter, rassistischer und antismetischer Gewalt. Wir
+              wollen offen legen: diese Gewalt passiert überall. Wenn du wissen
+              willst wo, dann nutze unser <a href="/karte">Recherche-Tool</a>.
             </p>
           </ContentMiddle>
         </section>
@@ -190,41 +197,57 @@ function Home({ content }) {
           <ContentMiddle>
             <h4 className="title is-4">Recherche-Tool</h4>
             <p>
-              Mit Tatort Rechts ist es möglich, die öffentlich verfügbaren Daten
-              zu rechter Gewalt von 12 Organisationen systematisch und gezielt
-              zu durchsuchen. Das geht über reinzoomen in eine
-            </p>
-            <p>
-              Karte oder über ein Suchfeld, das Ergebnisse für dich filtert.
-              Seit dem Jahr 2000 haben die 12 Organisationen zusammen etwa
-              16.000 Fälle von rechter Gewalt registriert.
+              Zum ersten Mal ist es möglich, die öffentlich verfügbaren Daten
+              von <a href="/blog/chroniken">12 Projekt-Seiten</a> systematisch
+              zu durchsuchen. Das hat es so bisher noch nicht gegeben. Du kannst
+              zum einen nach Fällen auf der <a href="/karte">Karte</a> suchen
+              und dich in dein Bundesland bis zu deiner Stadt reinzoomen. Du
+              kannst auch nach Schlagworten suchen, nach bestimmten Namen oder
+              Taten. Mit erweiterten Suchfiltern kannst du deine Suche weiter
+              eingrenzen.
             </p>
           </ContentMiddle>
         </section>
         <section className="hero">
           <ContentMiddle>
-            <h4 className="title is-4">Mach mit!</h4>
+            <h4 className="title is-4">Gezielte Suchen</h4>
             <p>
-              Tatort Rechts ist ein Prototyp, das Recherche-Tool ein erster
-              Schritt. Wie du siehst, fehlen uns noch Daten aus mehreren
-              Bundesländern. Wenn du Kenntnis über öffentlich bekannte Vorfälle
-              rechter Gewalt hast, dann kontaktier uns, damit wir weitere Daten
-              ergänzen können. Wie genau, sagen wir dir hier (LINK einfügen zu
-              Text auf Blog).
+              Seit dem Jahr 2000 sind auf den 12 Projekt-Seiten zusammen etwa
+              16.000 Fälle registriert worden, die einen rechten, rassistischen
+              oder antisemitischen Bezug haben. Die Erfassung{" "}
+              <a href="/blog/daten">Daten sind sehr heterogen</a>, aber uns geht
+              es nicht um Vergleichbarkeit oder eine statistische Analyse der
+              Daten. Mit Tatort Rechts wollen wir es User*innen möglich machen,
+              gezielt die erfassten Vorfälle durchsuchen zu können.
             </p>
           </ContentMiddle>
         </section>
+        <section className="hero">
+          <ContentMiddle>
+            <h4 className="title is-4">Zusammenhänge sehen</h4>
+            <p>
+              Wir hoffen, dass es nun durch die visuell ansprechend und
+              filterbar aufbereiteten Daten viel leichter sein wird,
+              Zusammenhänge in den Daten zu erkennen. In den Daten sind
+              Kundgebungen von Pegida oder Veranstaltungen der AfD.
+              Brandanschläge auf Autos von Menschen, die einen
+              Migrationshintergrund haben oder sich für Flüchtlinge einsetzen.
+              Aber auch Todesdrohungen und körperliche Angriffe mit Messern und
+              Waffen.
+            </p>
+          </ContentMiddle>
+        </section>
+
         <ParallaxBanner
           className="your-class"
           layers={[
             {
-              image: "/tor_logo.svg",
+              image: "/images/Tatort_Rechts_Symbolfoto_Zusammenhänge.jpg",
               amount: 0.5,
             },
           ]}
           style={{
-            width: "100%",
-            // height: "30rem",
+            height: "500px",
           }}
         ></ParallaxBanner>
 
@@ -232,19 +255,23 @@ function Home({ content }) {
           <ContentMiddle>
             <h4 className="title is-4">Waffen und Hakenkreuz</h4>
             <p>
-              STELL dir vor du steigst Abends aus dem Bus aus und plötzlich
-              greift dich ein Mann an. Er schlägt dich. Und dann eskaliert die
-              Situation, der Mann zieht eine Waffe und zielt auf dich.
-            </p>
-            <p>Das ist kein Gedankenspiel.</p>
-          </ContentMiddle>
-        </section>
-        <section className="hero">
-          <ContentMiddle>
+              Hier ein Beispiel. Stell dir vor du steigst Abends aus dem Bus aus
+              und plötzlich greift dich ein Mann an. Er schlägt dich. Und dann
+              eskaliert die Situation, der Mann zieht eine Waffe und zielt auf
+              dich.
+            </p>{" "}
+            <br />
+            <p>Das ist kein Gedankenspiel.</p> <br />
             <p>
-              Es ist tatsächlich passiert, in Bremen-Vegesack. Der Mann mit der
-              Waffe wurde später von der Polizei festgenommen, in seiner Wohnung
-              wurde neben Drogen und weiteren Waffen ein Hakenkreuz aus Metall
+              <a href="/tat/a2xlaW5lcmFuZG5vdGl6LTI0Mg==">
+                Es ist tatsächlich passiert
+              </a>
+              , in Bremen-Vegesack. Der Mann mit der Waffe wurde später von der
+              Polizei festgenommen, in seiner Wohnung wurde neben Drogen und
+              weiteren Waffen{" "}
+              <a href="/https://www.weser-kurier.de/region/die-norddeutsche_artikel,-intensivtaeter-bedroht-36jaehrigen-_arid,1952655.html">
+                ein Hakenkreuz aus Metall
+              </a>{" "}
               gefunden.
             </p>
           </ContentMiddle>
@@ -266,7 +293,7 @@ function Home({ content }) {
           <ContentMiddle>
             <h4 className="title is-4">Gefahr von Rechtsradikalismus</h4>
             <p>
-              IN dem Fall aus Bremen-Vegesack ist niemand tödlich verletzt
+              In dem Fall aus Bremen-Vegesack ist niemand tödlich verletzt
               worden. Doch wie gefährlich rechtsradikale Ideologien oder
               rassistische Überzeugungen werden können, zeigen die jünsten
               Ereignisse: Die Anschläge in Hanau, Halle und der Mord am Kasseler
@@ -275,19 +302,39 @@ function Home({ content }) {
             </p>
           </ContentMiddle>
         </section>
+
+        <ParallaxBanner
+          className="your-class"
+          layers={[
+            {
+              image: "/images/Tatort_Rechts_Symbolfoto_Kerzen.jpg",
+              amount: 0.5,
+            },
+          ]}
+          style={{
+            height: "500px",
+          }}
+        ></ParallaxBanner>
+
         <section className="hero">
           <ContentMiddle>
             <h4 className="title is-4">19. Februar 2020 - Hanau</h4>
             <p>
-              BEI dem Anschlag in Hanau starben neun Menschen mit
+              Bei dem Anschlag in Hanau starben neun Menschen mit
               Migrationshintergrund durch Schüsse des Täters Tobias R. Laut
               einem Gutachten für die Generalbundesanwaltschaft sollen bei
               Tobias R. Anzeichen für eine paranoide Schizophrenie und darauf
               aufgesetzt, eine “rechtsradikale Ideologie” erkennbar gewesen
-              sein, so berichtet es der Spiegel. In der Tatnacht konnten
-              offenbar Notrufe von der Polizei nicht entgegengenommen werden.
-              Der Notruf der Polizei war unterbesetzt, wie Hessens Innenminister
-              einräumte.
+              sein, so berichtet es{" "}
+              <a href="/https://www.spiegel.de/panorama/justiz/hanau-anschlag-neues-gutachten-zum-taeter-psychisch-krank-und-ein-rassist-a-00000000-0002-0001-0000-000174211404">
+                der Spiegel
+              </a>
+              . In der Tatnacht konnten offenbar Notrufe von der Polizei nicht
+              entgegengenommen werden. Der{" "}
+              <a href="/https://www.hessenschau.de/panorama/beuth-raeumt-notruf-panne-in-hanauer-tatnacht-ein,beuth-notrufpanne-hanau-100.html">
+                Notruf der Polizei
+              </a>{" "}
+              war unterbesetzt, wie Hessens Innenminister einräumte.
             </p>
           </ContentMiddle>
         </section>
@@ -296,12 +343,18 @@ function Home({ content }) {
           <ContentMiddle>
             <h4 className="title is-4">9. Oktober 2019 - Halle</h4>
             <p>
-              ZUM höchsten jüdischen Feiertag Jom Kippur versuchte ein
+              Zum höchsten jüdischen Feiertag Jom Kippur versuchte ein
               27-Jähriger zu den Feierlichkeiten gewaltsam in eine Synagoge in
-              Halle einzudringen. Als das nicht gelang erschoss er wahllos zwei
-              Menschen, seine Tat streamte er live ins Internet. Für den
-              rechtsterroristischen Anschlag wurde gegen den Attentäter die
-              Höchststrafe verhängt.
+              Halle einzudringen. Als das nicht gelang{" "}
+              <a href="https://www.mdr.de/sachsen-anhalt/halle/halle/urteil-halle-anschlag-lebenslange-haft-fuer-attentaeter-nach-schuessen-auf-synagoge-100.html">
+                erschoss er wahllos zwei Menschen
+              </a>
+              , seine Tat streamte er live ins Internet. Für den
+              rechtsterroristischen Anschlag wurde gegen den{" "}
+              <a href="/https://www.tagesschau.de/inland/rechtsextremismus/halle-attentat-urteil-101.html">
+                Attentäter die Höchststrafe
+              </a>{" "}
+              verhängt.
             </p>
           </ContentMiddle>
         </section>
@@ -309,48 +362,66 @@ function Home({ content }) {
           <ContentMiddle>
             <h4 className="title is-4">1. Juni 2019 - Istha</h4>
             <p>
-              AUS nächster Nähe ist der Kasseler Regierungspräsident Walter
+              Aus nächster Nähe ist der Kasseler Regierungspräsident Walter
               Lübcke erschossen worden. Lübcke wurde leblos auf der Terrasse
-              seines Wohnhauses gefunden. Die Tat gilt als erster
-              rechtsextremistischer Mord an einem Politiker in der
-              Bundesrepublik Deutschland. Im Jahr 2021 ist der Täter Stephan E.
-              verurteilt worden, alle Beteiligten legten Revision ein.
+              seines Wohnhauses gefunden. Die Tat gilt als{" "}
+              <a href="/https://www.stern.de/politik/deutschland/moerder-von-walter-luebcke-zu-lebenslanger-haft-verurteilt-30353688.html">
+                erster rechtsextremistischer Mord
+              </a>{" "}
+              an einem Politiker in der Bundesrepublik Deutschland. Im Jahr 2021
+              ist der Täter Stephan E. verurteilt worden, alle Beteiligten
+              legten{" "}
+              <a href="https://www.zeit.de/gesellschaft/zeitgeschehen/2021-02/mordfall-walter-luebcke-urteil-prozess-revision-familie-freispruch-nebenklaeger">
+                Revision
+              </a>{" "}
+              ein.
             </p>
           </ContentMiddle>
         </section>
-        {/* <ContentMiddle>
-          <h4 className="title is-4">Recherche-Tool</h4>
-          <p>
-            Auf dieser Webseite “Tatort Rechts”, wollen wir über rechte
-            Gewalttaten informieren. Wir haben dafür ein Recherche-Tool
-            entwickelt, bei dem User*innen Daten zu rechten Gewalttaten gezielt
-            durchsuchen können. Das hat es so bislang noch nicht gegeben. All
-            diese Daten sind bislang auf einzelnen Seiten verstreut gewesen und
-            dazu noch in einem Format, das eine systematische Suche unmöglich
-            gemacht hat.
-          </p>
-        </ContentMiddle>
-        <ContentMiddle>
-          <h4 className="title is-4">Nach Zusammenhängen suchen</h4>
-          <p>
-            Unser Recherche-Tool bietet dir zwei Möglichkeiten der Suche. Das
-            geht zum einen über eine Karte, auf der die Daten gemappt sind und
-            du reinzoomen kannst. Und außerdem über ein Suchfeld, bei dem du
-            anhand von Schlagworten oder Ortangaben die Ergebnisse filtern
-            kannst. Wenn du es noch nicht entdeckt hast, hier geht es zu unserem
-            Recherche-Tool.
-          </p>
-        </ContentMiddle>
-        <ContentMiddle>
-          <h4 className="title is-4">Mehr als tausend Taten</h4>
-          <p>
-            Rechte Gewalt ist real. Sie passiert in Deutschland, überall. Doch
-            nur in acht Bundesländern wird sie durch Opferberatungsstellen
-            erfasst. Der Verband dieser Beratungsstellen, der VBRG e. V., zählte
-            im Jahr 2019 1.347 rechts, rassistisch und antisemitisch motivierte
-            Angriffe.
-          </p>
-        </ContentMiddle> */}
+        <ParallaxBanner
+          className="your-class"
+          layers={[
+            {
+              image: "/tor_logo.svg",
+              amount: 0.5,
+            },
+          ]}
+          style={{
+            width: "100%",
+            // height: "30rem",
+          }}
+        ></ParallaxBanner>
+        <section className="hero">
+          <ContentMiddle>
+            <h4 className="title is-4">Vielzahl an Fällen</h4>
+            <p>
+              Vielzahl an Fällen Dies ist nur eine kurze Erinnerung an die
+              jüngsten Fälle. Es reihen sich viele weitere ein, wie etwa die
+              Mordserie der NSU-Terrorzelle. Die Liste der Fälle ist jedoch viel
+              umfangreicher. Laut der{" "}
+              <a href="https://www.amadeu-antonio-stiftung.de/rassismus/todesopfer-rechter-gewalt/">
+                Amadeu Antonio Stiftung
+              </a>{" "}
+              starben seit der Wende 213 Menschen durch rechte Gewalt (Stand
+              15.01.2021).
+            </p>
+          </ContentMiddle>
+        </section>
+        <section className="hero">
+          <ContentMiddle>
+            <h4 className="title is-4">Du kannst etwas tun</h4>
+            <p>
+              Wir wollen mit <a href="/blog">Tatort Rechts</a> einen Schwerpunkt
+              setzen auf rechte Gewalt und diese sichtbar machen. Dieses Projekt
+              ist ein Prototyp, das Recherche-Tool ein erster Schritt. Wie du
+              auf der Karte sehen kannst, fehlen uns noch{" "}
+              <a href="/blog/daten">Daten</a> aus mehreren Bundesländern. Wenn
+              du Kenntnis über öffentlich bekannte Vorfälle rechter,
+              rassistischer oder antisemitischer Gewalt hast, dann{" "}
+              <a href="/blog/mach-mit">mach mit</a> und ergänze weitere Daten.
+            </p>
+          </ContentMiddle>
+        </section>
       </ParallaxProvider>
       <Footer />
     </>
