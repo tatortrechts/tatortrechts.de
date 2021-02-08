419f2388cbbfc1addd478c1cb13ab3dbabf96c15
Johannes Filter <hi@jfilter.de>
2021-02-08 02:27:05 +0100

    add important headers

--- next.config.js
+++ next.config.js
@@ -9,6 +9,27 @@ module.exports = {
             key: "Cache-Control",
             value: "public, max-age=900, stale-while-revalidate=3600",
           },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "efault-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "deny",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
         ],
       },
     ];
